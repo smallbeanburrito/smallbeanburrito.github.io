07c7e62c91d109edacb7055b514a2586f911b203
Keith Cao <97124773+smallbeanburrito@users.noreply.github.com>
2022-04-13 02:36:36 -0400

    Add files via upload
    final version WEB222 final assignment

--- app.js
+++ app.js
@@ -2,16 +2,29 @@ var form = document.querySelector('#contactform');
 
 function salary() {
 var choices = Array.from(form.purpose);
+var purpose;
 choices.forEach(function(choice) {
     if(choice.checked && choice.value == "hiring") {
-        console.log(choice.value);
+        purpose = choice.value;
         document.getElementById("salary").style.display = "block";
     }
     else if (choice.checked){
+        purpose = choice.value;
         document.getElementById("salary").style.display = "none";
     }
 });
+return purpose;
 }
 
+function validate() {
+    var pay = form.rate;
+    console.log(salary);
+    if (salary() == "hiring") {
+        return pay.value != null && pay.value > 14
+    }
+    else return true;
+}
+
+form.onsubmit = validate;
 form.addEventListener('click', salary);
 
